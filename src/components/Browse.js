59ca81f847fc33ecf3ac5136df0688c02494c0eb
Rohit Saini <rohitsaini0444@gmail.com>
2026-01-23 14:51:05 +0530

    Fetch list of now playing movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { MOVIE_LIST_API_OPTIONS } from '../utils/constants';
 
 function Browse() {
+
+  const getNowPlayingMoviesList = async () => {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/movie/changes?page=1', MOVIE_LIST_API_OPTIONS);
+      const data = await res.json();
+      console.log("Data", data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getNowPlayingMoviesList();
+  }, [])
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,10 @@
 export const APP_LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-01-09/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const AVATAR_URL = "https://occ-0-6247-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABauoHATTBM_Co69wepwo2EoiXCaTfXBeMBenvD8EJjOA9AO6FX-zsUXpNuOTkujMZDili6BLc1sxlFMjENPrbyoxoII3rT6sGKa7WcjZdQs5JH_aB3bqDVCvUMiIwb1DA36wRxg3eQ5VXQ.png?r=e6e";
 export const BROWSE_BACKGROUND_IMAGE_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/ce462eb6-4d7f-4c9a-9f61-93cb535a64fd/web/IN-en-20260105-TRIFECTA-perspective_5ec818ea-11f4-4bff-a409-8f36e9f9a1e2_large.jpg"
+export const MOVIE_LIST_API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Y2FiNGJmOTc3ZWYyZjI4YWFjY2JkNDhiMjY4YjIyNSIsIm5iZiI6MTc2OTE1NjAzNS4zMDIsInN1YiI6IjY5NzMyZGMzOTJiYWIyNjFhN2UzZDZlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6JAOupdjGwMfpy35FSzHTOvnY2Mj0sKVxg-FkqWn5sY'
+    }
+};
