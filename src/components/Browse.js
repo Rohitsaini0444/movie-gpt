9f6da07bece87f3a860c93f81fe7753c1d7ff3bf
Rohit Saini <rohitsaini0444@gmail.com>
2026-01-23 15:28:30 +0530

    Used custom hooks for fetching movies and updating store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,27 +1,8 @@
-import React, { useEffect } from 'react'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import Header from './Header'
-import { MOVIE_LIST_API_OPTIONS } from '../utils/constants';
-import { useDispatch } from 'react-redux';
-import {addMovies} from '../utils/movieSlice'
 
 function Browse() {
-  const dispatch = useDispatch();
-
-  const getNowPlayingMoviesList = async () => {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', MOVIE_LIST_API_OPTIONS);
-      const data = await res.json();
-      dispatch(addMovies(data))
-      console.log("Data", data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    getNowPlayingMoviesList();
-  }, [])
-
+ useNowPlayingMovies();
   return (
     <div>
       <Header />
---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../utils/movieSlice";
+import { MOVIE_LIST_API_OPTIONS } from "../utils/constants";
+
+const useNowPlayingMovies = () => {
+    const dispatch = useDispatch();
+
+    const getNowPlayingMoviesList = async () => {
+        try {
+            const res = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', MOVIE_LIST_API_OPTIONS);
+            const data = await res.json();
+            dispatch(addMovies(data))
+            console.log("Data", data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getNowPlayingMoviesList();
+    }, [])
+
+}
+
+export default useNowPlayingMovies;
